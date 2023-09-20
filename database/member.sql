3253b94a2d3fb1f3eb29aefb5f5962caed03bc5d
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-09-21 00:17:24 +0900

    Add files via upload

---
+++ database/member.sql
@@ -0,0 +1,15 @@
+-- 회원 테이블 생성
+CREATE TABLE member (
+  idx NUMBER PRIMARY KEY,
+  id VARCHAR2(10) NOT NULL,
+  pw VARCHAR2(20) NOT NULL,
+  nickname VARCHAR2(10) NOT NULL,
+  grade VARCHAR2(10) CHECK (grade IN ('MANAGER', 'ASSOCIATE', 'STAFF', 'GENERAL'))
+);
+
+-- 관리자 계정 생성
+INSERT INTO member VALUES (1, 'admin', '1234', '관리자', 'MANAGER');
+COMMIT;
+
+-- 확인
+SELECT * FROM member;
