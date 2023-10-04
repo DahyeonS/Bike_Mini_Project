33b94c80555b27dff56db73c97bba512348ffe44
skyteme <145546791+skyteme@users.noreply.github.com>
2023-10-04 12:12:18 +0900

    경로수정
    경로수정

--- temp/정민석/main/webapp/member/script/delete.js
+++ temp/정민석/main/webapp/member/script/delete.js
@@ -9,7 +9,7 @@ function deleteJson() {
         success: function(data) {
             if(data['rs'] === 1) {
                 alert('회원탈퇴가 완료되었습니다. 그동안 감사했습니다.');
-                location.href = 'index.do';
+                location.href = '../home/index.do';
             } else alert('죄송합니다. 다시 시도해주세요.');
         }, error: function(xhr, status, error) {
             console.log(xhr, status, error);
