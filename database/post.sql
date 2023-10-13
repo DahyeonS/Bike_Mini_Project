7f6139d4374287a1d49ca80ed5dc85e90c505eac
Dahyeon Seo <siren199704@gmail.com>
2023-10-13 12:59:10 +0900

    닉네임 글자 수 변경

--- database/post.sql
+++ database/post.sql
@@ -2,7 +2,7 @@
 CREATE TABLE post (
     id VARCHAR2(10) REFERENCES member(id) ON DELETE CASCADE NOT NULL,
     num NUMBER PRIMARY KEY NOT NULL,
-    nickname VARCHAR2(10) REFERENCES member(nickname) NOT NULL,
+    nickname VARCHAR2(20) REFERENCES member(nickname) NOT NULL,
     title VARCHAR2(50) NOT NULL,
     context VARCHAR2(4000) NOT NULL,
     category VARCHAR2(10) NOT NULL,
